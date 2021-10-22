0a1ea03881dbe83f46623c61c09b9e8f8a672617
xuhaitao <xuhaitao@9fbank.com.cn>
2021-10-22 10:51:03 +0800

    抽取基础gradle，修改buildSrc

---
+++ buildSrc/src/main/kotlin/Android.kt
@@ -0,0 +1,13 @@
+/**
+ * 管理AdnroidX相关依赖
+ * https://blog.csdn.net/maguifen2012/article/details/118576742
+ */
+object Android {
+
+    const val appcompat         = "androidx.appcompat:appcompat:1.3.1"
+    const val coreKtx           = "androidx.core:core-ktx:1.6.0"
+    const val material          = "com.google.android.material:material:1.4.0"
+
+    const val constraintlayout  = "androidx.constraintlayout:constraintlayout:2.1.0"
+
+}
